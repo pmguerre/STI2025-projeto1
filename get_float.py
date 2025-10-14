956444a678503a312cb5208fda59e4e8018dd8a7
Pedro Guerreiro <pmguerre@ualg.pt>
2025-10-14 14:39:41 +0100

    movi as função para uma livraria e corrigi o ficheiro principal

--- get_float.py
+++
@@ -1,3 +0,0 @@
-def get_float(s):
-    x = input(s)
-    return float(x)
--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
 def get_int(msg):
     i = int(input(msg))
     return i
+
+def get_float(s):
+    x = input(s)
+    return float(x)
